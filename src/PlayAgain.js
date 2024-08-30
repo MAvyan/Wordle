c247b73f8de4533472b3efd060ef057ebd4bb718
MAVINGAyannis <y.mavinga.pro@gmail.com>
2024-08-30 09:00:10 +0200

    typo correction

--- src/PlayAgain.js
+++ src/PlayAgain.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PlayAgain = ({ onClick }) => {
   return (
     <>
-      <Button onClick={onClick}>Play Again?</Button>
+      <Button onClick={onClick}>Play Again ?</Button>
     </>
   );
 };
